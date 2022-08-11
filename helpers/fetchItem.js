3e2bd6827d9ea40ed3146bd9544377fb93dfd3e8
marceslguima <marcelsguima@gmail.com>
2022-08-11 20:46:34 -0300

    teste 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,10 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (query) => {
+  const url = `https://api.mercadolibre.com/items/${query}`;
+
+  const api = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data);
+    return api;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,23 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
-});
+  
+  it('Teste se fetchItem é uma função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+
+  it('Execute a função fetchItem com o argumento computador e teste se fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao chamar a função fetchItem com o argumento computador, a função fetch utiliza o endpoint correto', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+  });
+
+  it('Teste se o retorno da função fetchItem com o argumento computador é uma estrutura de dados igual ao objeto item', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+
+  });
